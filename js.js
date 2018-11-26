f10663f2e4df3cf0befc7d1a9062102e6bb8ed6b
project2401 <igor.ryabenko@qbex.io>
2018-11-26 11:43:40 +0200

    kalkulator work

--- js.js
+++ js.js
@@ -1,13 +1,61 @@
-// var wrapper = document.querySelector('.wrapper');
-// var arr = '<button id="'+0+'">'+0+'</button>'; 
-// for(var i =0; i<9;i++){
-//     var count =  i + 1 
-//     var button = '<button id="'+count+'">'+ count+'</button>';
-//     var arr = arr + button;
-// }
-// wrapper.innerHTML = arr;
 
+var numbers = document.querySelectorAll(".number");
+var result = document.querySelector(".result");
+var leftOperation = null;
+var operation = null;
+var rightOperation = null;
+// console.log(numbers);
 
+numbers.forEach(el => {
+    el.addEventListener('click', function(){
+      if(Number.isInteger(+this.innerHTML)){
+        if(+result.innerHTML === 0){
+            result.innerHTML = this.innerHTML;
+        }else {
+            result.innerHTML = result.innerHTML + this.innerHTML;
+            rightOperation = this.innerHTML;
+        }
+    }
+    if(this.innerHTML ==='C'){
+        result.innerHTML = 0;
+    }
+    if(this.innerHTML === '+' ||
+        this.innerHTML === '-' ||
+        this.innerHTML === '/' ||
+        this.innerHTML === '*')
+        {
+            leftOperation = result.innerHTML;
+            operation = this.innerHTML;
+            result.innerHTML = leftOperation + this.innerHTML;
+            // console.log(curentOperation);
+            
+    }
+    if(this.innerHTML === '='){
+        console.log(rightOperation);
+        
+       var resultOperation;
+       switch(operation){
+           case '+':
+           resultOperation = +leftOperation + +rightOperation;
+           break;
+           case '-':
+           resultOperation = +leftOperation - +rightOperation;
+           break;
+           case '*':
+           resultOperation = +leftOperation * +rightOperation;
+           break;
+           case '/':
+           resultOperation = +leftOperation / +rightOperation;
+           break;
+       }
+       
+       result.innerHTML = resultOperation;
+
+       leftOperation = null;
+       operation = null;
+    }
+});  
+    })
     
 
 
