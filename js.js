c61a439782c20038cdc75a5203872dc1be320922
project2401 <igor.ryabenko@qbex.io>
2018-11-26 10:18:20 +0200

    add markup

--- js.js
+++ js.js
@@ -1,8 +1,13 @@
-var wrapper = document.querySelector('.wrapper');
-var arr = '<button id="'+0+'">'+0+'</button>'; 
-for(var i =0; i<9;i++){
-    var count =  i + 1 
-    var button = '<button id="'+count+'">'+ count+'</button>';
-    var arr = arr + button;
-}
-wrapper.innerHTML = arr;
+// var wrapper = document.querySelector('.wrapper');
+// var arr = '<button id="'+0+'">'+0+'</button>'; 
+// for(var i =0; i<9;i++){
+//     var count =  i + 1 
+//     var button = '<button id="'+count+'">'+ count+'</button>';
+//     var arr = arr + button;
+// }
+// wrapper.innerHTML = arr;
+
+
+    
+
+
